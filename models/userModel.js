d6b709f0f44a45a984e850987a9b6c6a3d82fb2d
MostafaMosaad <101674967+MostafaMosaad@users.noreply.github.com>
2023-03-01 00:31:17 +0200

    Update userModel.js

--- models/userModel.js
+++ models/userModel.js
@@ -14,7 +14,6 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
         unique: true,
-        match: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
         minlength: 5
     },
     password: {
